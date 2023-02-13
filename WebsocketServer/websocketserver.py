d37d11407bdac307c3e73267a9568e3fea6e48df
Mark Oakley <mark@spire-web.co.uk>
2023-02-13 21:26:42 +0000

    start wheel speeds being sent to firmare rather than calculated in firmware

--- WebsocketServer/websocketserver.py
+++ WebsocketServer/websocketserver.py
@@ -3,18 +3,48 @@ import json
 import websockets
 import paho.mqtt.client as mqtt
 
+def calculate_wheel_speeds(data):
+    return {
+        "left" : 0,
+        "right" : 0
+    }
+
+def generate_buggycall(data):
+    deps = {
+                "h" : data['h'],
+                "r" : data['r'],
+                "m" : data['m']
+            }
+    
+    # we're now decoupling the heading and radians from the actual speed and direction of the buggy.
+    # the new format for the buggy messages are 'l' and 'r'
+    # with 'l' being the forward velocity of the left hand wheel
+    # and 'r' being the forward velocity of the right hand wheel
+    # so that we're further decoupling, the range is from -1 for full reverse, through 0 for stationary to 1 for full forwards.
+    
+    
+    
+    new = calculate_wheel_speeds(data)
+    
+    
+    return {**deps, **new}
+
 async def handle(websocket, path):
     while True:
         try:
             message = await websocket.recv()
             data = json.loads(message)
-            print(f"received: r={data['r']}, m={data['m']}, h={data['h']}")
+            # print(f"received: r={data['r']}, m={data['m']}, h={data['h']}")
             
-            sendData = {
-                "h" : data['h'],
-                "r" : data['m'],
-                "m" : data['r']
-            }
+            #sendData = {
+            #    "h" : data['h'],
+            #    "r" : data['r'],
+            #    "m" : data['m']
+            #}
+            
+            sendData = generate_buggycall(data)
+            
+            print(json.dumps(sendData))
             client.publish("buggy", json.dumps(sendData))
 
         except websockets.exceptions.ConnectionClosed:
