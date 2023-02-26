c96a06191024915f0e51aab49ec6b40a6907a6bf
Mark Oakley <mark@spire-web.co.uk>
2023-02-26 16:47:14 +0000

    creating unit tests for the conversion of circular to square

---
+++ WebsocketServer/modules/rangeofmotion/rangeofmotion.py
@@ -0,0 +1,5 @@
+def CircularToSquare():
+    pass
+
+def SquareToCircular():
+    pass
---
+++ WebsocketServer/tests/test_CircularToSquare.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('../modules/rangeofmotion/')
+
+import unittest
+import math
+
+import rangeofmotion
+
+class tCircularToSquareTests(unittest.TestCase):
+
+    def test_extremeties_return_full_x_or_y(self):
+        test_cases = [
+            (0, 0, 1),
+            ((math.pi / 4), 1, 0),
+            ((math.pi) / 2, -1, 0),
+            ((-math.pi) / 4, 0, -1)
+        ]
+        
+        for theta, expected_x, expected_y in test_cases:
+            with self.subTest(theta = theta, expected_x = expected_x, expected_y = expected_y):
+                
+                magnitude = 1
+                actual_x, actual_y = rangeofmotion.circular_to_square(theta, magnitude)
+                self.assertEqual(expected_x, actual_x)
+                self.assertEqual(expected_y, actual_y)
+    
+    def diagonals_return_extreme_x_andy(self):
+        self.assertEqual(1, 2)
