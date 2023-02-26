f28ba1878fd11093505ebfd4b6e7cff5452aa706
Mark Oakley <mark@spire-web.co.uk>
2023-02-26 17:58:31 +0000

    guard clauses added to the mapping

--- WebsocketServer/modules/rangeofmotion/rangeofmotion.py
+++ WebsocketServer/modules/rangeofmotion/rangeofmotion.py
@@ -3,3 +3,25 @@ def circular_to_square(theta, magnitude):
 
 def SquareToCircular():
     pass
+
+
+
+def map_circular_magnitude_to_circumstribed_square(circular_magnitude, outer_magnitude_upper_range):
+    """
+    Maps a smaller magnitude from a circle to a larger one on the square circumscribed to that circle.
+    
+    Args:
+        inner_magnitude: magnitude of a joystick in the range 0 to 1.
+        outer_magnitude_upper_range: the upper range of the out magnitude.
+        
+    Returns:
+        the mappings 
+    """
+    
+    if circular_magnitude > 1:
+        raise Exception(f'circular magnitude must be <= 1.  Actual value was { circular_magnitude }')
+    
+    if circular_magnitude < 0:
+        raise Exception(f'circular magnitude must be > 0.  Actual value was { circular_magnitude }')
+    
+    return (circular_magnitude * outer_magnitude_upper_range)
--- WebsocketServer/tests/test_CircularToSquare.py
+++ WebsocketServer/tests/test_CircularToSquare.py
@@ -1,18 +1,40 @@
 import sys
-sys.path.append('../modules/rangeofmotion/')
-
 import unittest
 import math
 
+sys.path.append('../modules/rangeofmotion/')
 import rangeofmotion
 
 class tCircularToSquareTests(unittest.TestCase):
+    
+    def test_maping_of_numbers(self):
+        test_cases = [
+            (1, 0.5, 0.5),
+            (2, 0.75, 1.5),
+            (2, 0.5, 1),
+        ]
+        
+        for high_bound, input, expected_result in test_cases:
+            with self.subTest(high_bound = high_bound, input = input, expected_output = expected_result):
+                actual_result = rangeofmotion.map_circular_magnitude_to_circumstribed_square(input, high_bound)
+                self.assertEqual(expected_result, actual_result)
+    
+    def test_out_of_range_input_throws(self):
+        test_cases = [
+            -0.1,
+            1.1,
+        ]
+        for value in test_cases:
+            with self.subTest(value = value):
+                self.assertRaises(Exception, rangeofmotion.map_circular_magnitude_to_circumstribed_square, value, 1)
+            
+    
 
-    def test_extremeties_return_full_x_or_y(self):
+    def xtest_extremeties_return_full_x_or_y(self):
         test_cases = [
             (0, 1, 0),
             #((math.pi / 4), 0, 1),
-            #((math.pi) / 2, -1, 0),
+            ((math.pi) / 2, -1, 0),
             #((-math.pi) / 4, 0, -1)
         ]
         
@@ -24,5 +46,5 @@ class tCircularToSquareTests(unittest.TestCase):
                 self.assertEqual(expected_x, actual_x)
                 self.assertEqual(expected_y, actual_y)
     
-    def diagonals_return_extreme_x_andy(self):
+    def dxiagonals_return_extreme_x_andy(self):
         self.assertEqual(1, 2)
